2ff3839014f9a16374672b04e42d0d1c9a2b11c9
IvanMi773 <imihailovskij7@gmail.com>
2021-04-18 16:21:38 +0300

    singleton

---
+++ src/com/company/creational_patterns/singelton/Main.java
@@ -0,0 +1,8 @@
+package com.company.creational_patterns.singelton;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+    }
+}
